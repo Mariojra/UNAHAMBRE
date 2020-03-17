deb38c70bdac483b916e70a7a97f93cef071cd9d
Lesman Nolasco <lesnolasco97@gmail.com>
2020-03-17 14:34:47 -0600

    Cambios en el controlador

--- unahambre/funcionalidades/controlador-negocio.js
+++ unahambre/funcionalidades/controlador-negocio.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
-    console.log("se cargo el doc");
+    console.log("Cargandp documento...");
+    
     // Step show event
     $("#smartwizard").on("showStep", function(e, anchorObject, stepNumber, stepDirection, stepPosition) {
        //alert("You are on step "+stepNumber+" now");
@@ -7,58 +8,55 @@ $(document).ready(function(){
            $(".sw-btn-prev").addClass('disabled');
            $(".btn-fin").addClass('display');
            $(".btn-cancel").addClass('display');
-           console.log("estas en el if first");
+           console.log(stepNumber);
        }else if(stepPosition === 'final'){
            $("#next-btn").addClass('disabled');
            $(".btn-fin").removeClass('display');
            $(".btn-cancel").removeClass('display');
            $(".sw-btn-prev").addClass('display');
            $(".sw-btn-next").addClass('display');
-           console.log("estas en el if final");
+           console.log(stepNumber);
        }else{
            $("#prev-btn").removeClass('disabled');
            $("#next-btn").removeClass('disabled');
            $(".btn-fin").addClass('display');
            $(".btn-cancel").addClass('display');
-           console.log("estas en el if else");
+           console.log(stepNumber);
        }
     });
 
-    $("#smartwizard").on("endReset", function() {
-      $("#next-btn").removeClass('disabled');
-    });
 
     // Toolbar extra buttons
-    var btnFinish = $('<button></button>').text('Finalizar')
-                                     .addClass('btn btn-info btn-fin').on('click',function(){
-                                        console.log('estas en el click de finalizar');
-                                        axios({
-                                            url:'http://localhost:3001/api/menus',
-                                            method:'GET'
-                                        }).then(res=>{
-                                            console.log(res);
-                                        })
-                                    });
-                                    //  .on('click', function(){ alert('SOLICITUD ENVIADA'); });
+    var btnFinish = $('<button></button>').text('Finalizar').addClass('btn btn-info btn-fin').on('click',function(){
+        console.log("click funciona");
+        var informacion ={
+            usuario:$("#n-usuario").val(),
+            contrasena:$("#n-password").val(),
+            nombre_local:$("#n-local").val(),
+            ubicacion:$("#n-ubicacion").val(),
+            telefono: $("#n-telefono").val(),
+            correo: $('#n-correo').val(),
+        };
+        console.log(informacion);
+        axios({
+            url:'http://localhost:3001/api/menus',
+            method:'GET'
+        }).then(res=>{
+            console.log(res);
+        })
+        alert('SOLICITUD ENVIADA')
+    });
+
     var btnCancel = $('<button></button>').text('Cancelar')
                                      .addClass('btn btn-danger btn-cancel')
                                      .on('click', function(){ $('#smartwizard').smartWizard("reset"); });
-//     $('.btn-fin').on('click',function(){
-//     console.log('estas en el click de finalizar');
-//     axios({
-//         url:'http://localhost:3001/api/menus',
-//         method:'GET'
-//     }).then(res=>{
-//         console.log(res);
-//     })
-// });
 
 
     // Smart Wizard
     $('#smartwizard').smartWizard({
             selected: 0,
             theme: 'arrows',
-            lang: {  // Language variables
+            lang: { 
                 next: 'Siguiente', 
                 previous: 'Anterior'
             },
@@ -67,6 +65,7 @@ $(document).ready(function(){
             hideButtonsOnDisabled: false,
             transitionSpeed: 400,
             showStepURLhash: true,
+            autoAdjustHeight: true,
             toolbarSettings: {toolbarPosition: 'down',
                               toolbarButtonPosition: 'end',
                               toolbarExtraButtons: [btnFinish, btnCancel
@@ -76,33 +75,8 @@ $(document).ready(function(){
                                 // alert('Your button click');
                                 //   })
                             
-                            ],
+                            ]
                             },
     });
-
-    $("#prev-btn").on("click", function() {
-        // Navigate previous
-        $('#smartwizard').smartWizard("prev");
-        return true;
-    });
-
-    $("#next-btn").on("click", function() {
-        // Navigate next
-        $('#smartwizard').smartWizard("next");
-        return true;
-    });
-    
-    // $('#wizard').smartWizard({
-    //     onFinish: function() {
-    //        $("form").submit();
-    //     }
-    // });
-    
-    // $("form").validate({
-    //     rules: {
-    //         username: "required"
-    //     }
-    // });
-
 });
 
