4801eeb9df254d618dafca5694263d489db71fa6
Josue Aguilar <josueaguilar1198@gmail.com>
2020-03-24 03:59:10 -0600

    responsive, arreglo de bug info repetida, cambio pp, edicion de info, inicio de cambio password

--- unahambre/funcionalidades/controlador-perfil.js
+++ unahambre/funcionalidades/controlador-perfil.js
@@ -1,17 +1,20 @@
+window.onload = info_usuarios();
+
 function info_usuarios(){
-    i = 0;
+  console.log('se entro a la funcion');
+    let i = 0;
 
     axios({
         method:'POST',
         url:'http://localhost:3001/api/info-user',
         data: {
-          idUsuario: 5
+          idUsuario: 7
         }
     }).then(res=>{
-        var div = $('#informacion-perfil');
+        let div = $('#info-perfil');
 
         const datos_perfil =res.data.items;
-            var tarjeta = `<div>
+            let tarjeta = `<div>
             <h2>INFORMACION PERSONAL</h2>
             <p></p>
             <div class="container-fluid foco">
@@ -19,68 +22,95 @@ function info_usuarios(){
                 <h3>MI PERFIL</h3>
                 <p>Mantené tu información siempre actualizada</p>
               </div>
-        
+
               <div class="subcontenedor row justify-content-center align-items-center">
-                <div class="parametros col-4"><h3>NOMBRE</h3></div>
-                <div class="col-4">${datos_perfil[i].Nombre} ${datos_perfil[i].Apellidos} </div>
-                <div class="col-4"><a href=""><i class="fas fa-edit"></i></a> </div>
+                <div class="parametros col-12 col-sm-4 col-md-4 col-lg-4 col-xl-4"><h3>NOMBRE</h3></div>
+                <div class="col-12 col-sm-8 col-md-4 col-lg-4 col-xl-4">${datos_perfil[i].Nombre} ${datos_perfil[i].Apellidos} </div>
+
               </div>
-        
+
               <div class="subcontenedor row justify-content-center align-items-center">
-                <div class="parametros col-4"><h3>usuario</h3></div>
-                <div class="col-4">${datos_perfil[i].Nombre_Usuario}</div>
-                <div  class="col-4"><a href=""><i class="fas fa-edit"></i></a> </div>
+                <div class="parametros col-12 col-sm-4 col-md-4 col-lg-4 col-xl-4"><h3>usuario</h3></div>
+                <div class="col-12 col-sm-8 col-md-4 col-lg-4 col-xl-4">${datos_perfil[i].Nombre_Usuario}</div>
+
               </div>
-        
+
               <div class="subcontenedor row justify-content-center align-items-center">
-                <div class="parametros col-4"><h3>SEXO</h3></div>
-                <div class="col-4">${datos_perfil[i].Sexo}</div>
-                <div class="col-4"><a href=""><i class="fas fa-edit"></i></a> </div>
+                <div class="parametros col-12 col-sm-4 col-md-4 col-lg-4 col-xl-4"><h3>SEXO</h3></div>
+                <div class="col-12 col-sm-8 col-md-4 col-lg-4 col-xl-4">${datos_perfil[i].Sexo}</div>
+
               </div>
-        
+
               <div class="subcontenedor row justify-content-center align-items-center">
-                <div class="parametros col-4"><h3>CONTRASEÑA</h3></div>
-                <div class="col-4"> * * * * * * * * *</div>
-                <div  class="col-4"><a href=""><i class="fas fa-edit"></i></a> </div>
+                <div class="parametros col-12 col-sm-4 col-md-4 col-lg-4 col-xl-4"><h3>CONTRASEÑA</h3></div>
+                <div class="col-12 col-sm-8 col-md-4 col-lg-4 col-xl-4"> * * * * * * * * *</div>
+
               </div>
             </div>
-            
+
             <div class="container-fluid foco">
               <div class="titulo">
                 <h3>CONTACTOS</h3>
               </div>
-        
+
               <div class="subcontenedor row justify-content-center align-items-center">
-                <div class="parametros col-4"><h3>correo</h3></div>
-                <div class="col-4">${datos_perfil[i].Correo}</div>
-                <div class="col-4"><a href=""><i class="fas fa-edit"></i></a> </div>
+                <div class="parametros col-12 col-sm-4 col-md-4 col-lg-4 col-xl-4"><h3>correo</h3></div>
+                <div class="col-12 col-sm-8 col-md-4 col-lg-4 col-xl-4">${datos_perfil[i].Correo}</div>
+
               </div>
-        
+
               <div class="subcontenedor row justify-content-center align-items-center">
-                <div class="parametros col-4"><h3>telefono</h3></div>
-                <div class="col-4">${datos_perfil[i].Celular}</div>
-                <div class="col-4"><a href=""><i class="fas fa-edit"></i></a> </div>
+                <div class="parametros col-12 col-sm-4 col-md-4 col-lg-4 col-xl-4"><h3>telefono</h3></div>
+                <div class="col-12 col-sm-8 col-md-4 col-lg-4 col-xl-4">${datos_perfil[i].Celular}</div>
+
               </div>
             </div>
 
                 </div>`;
         div.append(tarjeta);
-        console.log(datos_perfil);
+        $("#modal-txt-user").attr('value',datos_perfil[i].Nombre_Usuario);
+        $("#modal-txt-nombre").attr('value',datos_perfil[i].Nombre);
+        $("#modal-txt-apellido").attr('value',datos_perfil[i].Apellidos);
+        $("#modal-txt-tel").attr('value',datos_perfil[i].Celular);
+        // console.log(datos_perfil);
     }).catch(function(error){
         console.log(error);
-    });                 
-}
-
-info_usuarios();
+    });
+};
 
 
+// EJECUTAR LA VENTANA MODAL
+$("#btn-editar-info").click(function(){
+  console.log("ventana modal lanzada");
+  $("#editar-info").modal("show");
+})
 
-{/* <div >
-<p> ${datos_perfil.Nombre}</p> 
-<p> ${datos_perfil.Apellidos}</p>
-<p> ${datos_perfil.Nombre_Usuario}</p> 
-<p> ${datos_perfil.Celular}</p> 
-<p> ${datos_perfil.Sexo}</p> 
-<p> ${datos_perfil.Numero_Identidad}</p> 
-<p> ${datos_perfil.Correo}</p> 
-</div> */}
+
+//EDITAR INFO DE USUARIOS, BTN EN MODAL
+$("#btn-conf-editar").click(function(){
+  console.log("click en confirmar edicion");
+  let data ={
+    idUsuario: 7,//esto vendra en el sessionStorage.
+    nombreUsuario:"Mario Rosales",//esto vendra en el sessionStorage.
+    nuevoUsuario:$("#modal-txt-user").val(),
+    celular:$("#modal-txt-tel").val(),
+    nuevoNombre:$("#modal-txt-nombre").val(),
+    nuevoApellido:$("#modal-txt-apellido").val()
+  }
+  console.log("esta es la data"+data);
+  axios({
+    method:'PUT',
+    url:'http://localhost:3001/api/cambiar-info-usuario',
+    data: data
+  }).then(res=>{
+    console.log(res);
+    $("#editar-info").modal("hide");
+  }).catch(err=>{
+    console.log(err)
+  })
+});
+
+$("#btn-cambiar-contrasena").click(function(){
+  console.log("ventana modal lanzada");
+  $("#cambiar-contrasena").modal("show");
+});
--- unahambre/js/popper-perfil.js
+++ unahambre/js/popper-perfil.js
@@ -4,12 +4,68 @@ $("#menu-toggle").click(function(e) {
 });
 
 
-$(document).ready(function(e) {
+// $(document).ready(function(e) {
     $('#perfil').on('click', function(){
         $('#contenido-perfil').load('tablas-perfil.html', function(data) {
-            $(this).html(data);
+            // $(this).html(data);
         });
 
     })
 
+// });
+
+$("#pedidos").on('click',function () {
+    $('#contenido-perfil').load('en-proceso.html',function (data) {
+        
+    })
+})
+
+
+$("#pp-div").on('click',()=>{
+    $("#file-img").click();
+})
+
+//-----------------INTENTO 2 --------------------------------
+$("#file-img").change(function (e) {
+    let img = e.target.files[0];
+    // console.log(img);
+    let fd = new FormData();
+    fd.append("image",img);
+    // console.log(fd);
+    axios({
+        url:'http://localhost:3001/api/upload-profile-pic',
+        method: 'POST',
+        data: fd,
+        headers: {
+            'idUsuario': 7,
+        }
+    }).then(res=>{
+        console.log(res.data);
+        readURL(this);
+        //CODIGO FUNCIONAL, PROBLEMA CON RECORRIDO ENTRE CARPETAS DE WINDOWS
+        // let spdr = res.data.split('\\');
+        // console.log(spdr);
+        // let src = '../..';
+        // for (let i = 7; i < spdr.length; i++) {
+        //         src+='/'+spdr[i];
+        // }
+        // console.log(src);
+        // $('#pp-img').attr('src',src);
+    }).catch(err=>{
+        console.log(err);
+    });
 });
+
+
+//FUNCION UTILIZADA PARA PONER LA IMAGEN DE PERFIL
+function readURL(input) {
+    if (input.files && input.files[0]) {
+      var reader = new FileReader();
+      
+      reader.readAsDataURL(input.files[0]);
+      //esta funcion se ejecuta despues de que reader fue ejecutado como lector de url
+      reader.onload = function(e) {
+        $('#pp-img').attr('src', e.target.result);
+      }
+    }
+  }
