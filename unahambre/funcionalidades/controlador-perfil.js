1d8c087305697cec6e96a2ad27e847b7a0350955
Josue Aguilar <josueaguilar1198@gmail.com>
2020-03-25 02:47:17 -0600

    edicion perfil: responsive, bugs arreglados, edita la infode perfil y actualiza en tiempo real, cambio de contrasena con ventanas sweetalerts ya implementadas

--- unahambre/funcionalidades/controlador-perfil.js
+++ unahambre/funcionalidades/controlador-perfil.js
@@ -1,5 +1,22 @@
 window.onload = info_usuarios();
 
+//CONFIGURACION PREDETERMINADA SWEETALERTS
+const alert_default = Swal.mixin({
+  timer: 3000,
+  timerProgressBar: true,
+  onOpen: (modal)=>{
+      modal.addEventListener('mouseenter', Swal.stopTimer)
+      modal.addEventListener('mouseleave', Swal.resumeTimer)
+  },
+  showClass: {
+      popup: 'animated fadeInDown'
+    },
+    hideClass: {
+      popup: 'animated fadeOutUp'
+    }
+});
+
+//funcion al cargar pagina renderizada
 function info_usuarios(){
   console.log('se entro a la funcion');
     let i = 0;
@@ -25,13 +42,13 @@ function info_usuarios(){
 
               <div class="subcontenedor row justify-content-center align-items-center">
                 <div class="parametros col-12 col-sm-4 col-md-4 col-lg-4 col-xl-4"><h3>NOMBRE</h3></div>
-                <div class="col-12 col-sm-8 col-md-4 col-lg-4 col-xl-4">${datos_perfil[i].Nombre} ${datos_perfil[i].Apellidos} </div>
+                <div class="col-12 col-sm-8 col-md-4 col-lg-4 col-xl-4" id="div-nombre">${datos_perfil[i].Nombre} ${datos_perfil[i].Apellidos} </div>
 
               </div>
 
               <div class="subcontenedor row justify-content-center align-items-center">
                 <div class="parametros col-12 col-sm-4 col-md-4 col-lg-4 col-xl-4"><h3>usuario</h3></div>
-                <div class="col-12 col-sm-8 col-md-4 col-lg-4 col-xl-4">${datos_perfil[i].Nombre_Usuario}</div>
+                <div class="col-12 col-sm-8 col-md-4 col-lg-4 col-xl-4" id="div-usuario">${datos_perfil[i].Nombre_Usuario}</div>
 
               </div>
 
@@ -61,7 +78,7 @@ function info_usuarios(){
 
               <div class="subcontenedor row justify-content-center align-items-center">
                 <div class="parametros col-12 col-sm-4 col-md-4 col-lg-4 col-xl-4"><h3>telefono</h3></div>
-                <div class="col-12 col-sm-8 col-md-4 col-lg-4 col-xl-4">${datos_perfil[i].Celular}</div>
+                <div class="col-12 col-sm-8 col-md-4 col-lg-4 col-xl-4" id="div-celular">${datos_perfil[i].Celular}</div>
 
               </div>
             </div>
@@ -86,31 +103,125 @@ $("#btn-editar-info").click(function(){
 })
 
 
-//EDITAR INFO DE USUARIOS, BTN EN MODAL
+//EDITAR INFO DE USUARIOS, BTN EN EL MODAL
 $("#btn-conf-editar").click(function(){
   console.log("click en confirmar edicion");
-  let data ={
-    idUsuario: 7,//esto vendra en el sessionStorage.
-    nombreUsuario:"Mario Rosales",//esto vendra en el sessionStorage.
-    nuevoUsuario:$("#modal-txt-user").val(),
-    celular:$("#modal-txt-tel").val(),
-    nuevoNombre:$("#modal-txt-nombre").val(),
-    nuevoApellido:$("#modal-txt-apellido").val()
+  if(validarCampoVacio('#modal-txt-user')&&
+      validarCampoVacio('#modal-txt-nombre')&&
+      validarCampoVacio('#modal-txt-apellido')&&
+      validarCampoVacio('#modal-txt-tel')){
+    let data ={
+      idUsuario: 7,//esto vendra en el sessionStorage.
+      nombreUsuario:"Mario Rosales",//esto vendra en el sessionStorage.
+      nuevoUsuario:$("#modal-txt-user").val(),
+      celular:$("#modal-txt-tel").val(),
+      nuevoNombre:$("#modal-txt-nombre").val(),
+      nuevoApellido:$("#modal-txt-apellido").val()
+    }
+    // console.log("esta es la data"+data);
+    axios({
+      method:'PUT',
+      url:'http://localhost:3001/api/cambiar-info-usuario',
+      data: data
+    }).then(res=>{
+      // console.log(res);
+      document.getElementById('p-usuario').innerHTML=$("#modal-txt-user").val();
+      document.getElementById('div-usuario').innerHTML=$("#modal-txt-user").val();
+      document.getElementById('div-nombre').innerHTML=$("#modal-txt-nombre").val() + ' ' + $("#modal-txt-apellido").val();
+      document.getElementById('div-celular').innerHTML=$("#modal-txt-tel").val();
+      $("#editar-info").modal("hide");
+      alert_default.fire({
+        icon:"success",
+        title:"Informacion editada con exito."
+      });
+    }).catch(err=>{
+      console.log(err)
+    })
   }
-  console.log("esta es la data"+data);
-  axios({
-    method:'PUT',
-    url:'http://localhost:3001/api/cambiar-info-usuario',
-    data: data
-  }).then(res=>{
-    console.log(res);
-    $("#editar-info").modal("hide");
-  }).catch(err=>{
-    console.log(err)
-  })
 });
 
+//EJECUTAR VENTANA MODAL CAMBIAR CONTRASENA
 $("#btn-cambiar-contrasena").click(function(){
   console.log("ventana modal lanzada");
   $("#cambiar-contrasena").modal("show");
 });
+
+$('#btn-conf-contrasena').click(function(){
+  //usuario, contrasena, nueva_contrasena forma del json
+  let datos = {
+    usuario:7,//con el sessionStorage
+    contrasena: $('#modal-txt-contrasena').val(),
+    nueva_contrasena: $('#modal-txt-RNcontrasena').val()
+  };
+  // console.log(datos);
+  axios({
+    method:'POST',
+    url:'http://localhost:3001/api/cambiar-contrasena',
+    data: datos
+  }).then(res=>{
+    console.log(res);
+    if(res.data.error[1][0].mensaje==null){
+      console.log('contra cambiada');
+      alert_default.fire({
+        icon:'success',
+        title:'Contraseña cambiada con exito'
+      });
+    } else {
+      alert_default.fire({
+        icon: 'error',
+        title: res.data.error[1][0].mensaje
+      });
+    }
+    console.log(res.data.error[1][0].mensaje);
+  }).catch(err=>{
+    console.log(err);
+  });
+})
+
+//validar campos vacios
+function validarCampoVacio(id){
+  if ($(id).val()==""){
+      $(id).addClass("is-invalid");
+      $(id).removeClass("is-valid");
+      return false;
+  } else {
+      $(id).addClass("is-valid");
+      $(id).removeClass("is-invalid");
+      return true;
+  }
+}
+
+//VALIDANDO QUE LA CONTRASEÑA SEA FUERTE
+function validarContrasena(pwd,id) {
+  var rePassMedia= /^(?=\w*\d)(?=\w*[A-Z])(?=\w*[a-z])\S{8,16}$/;
+  var rePassFuerte = /^(?=.*\d)(?=.*[\u0021-\u002b\u003c-\u0040])(?=.*[A-Z])(?=.*[a-z])\S{8,16}$/;
+  if (rePassFuerte.test(pwd)) {
+      document.getElementById(id).classList.remove("is-invalid");
+      document.getElementById(id).classList.add("is-valid");
+      document.getElementById(id).classList.remove("is-medium");
+      return true;
+  } else if (rePassMedia.test(pwd)) {
+      document.getElementById(id).classList.remove("is-invalid");
+      document.getElementById(id).classList.remove("is-valid");
+      document.getElementById(id).classList.add("is-medium");
+      return false;
+  } else {
+      document.getElementById(id).classList.remove("is-valid");
+      document.getElementById(id).classList.add("is-invalid");
+      document.getElementById(id).classList.remove("is-medium");
+      return false;
+  }
+}
+
+//COMPARANDO LAS NUEVAS CONTRASENAS
+function compararContras(pwd2,id1,id2) {
+  if (pwd2==document.getElementById(id1).value) {
+      document.getElementById(id2).classList.add("is-valid");
+      document.getElementById(id2).classList.remove("is-invalid");
+      return true;
+  } else {
+      document.getElementById(id2).classList.add("is-invalid");
+      document.getElementById(id2).classList.remove("is-valid");
+      return false;
+  }
+}
