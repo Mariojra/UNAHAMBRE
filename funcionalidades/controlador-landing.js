351df37fd4c67df8d20cd7fc0549a169e9f4e713
Josue Aguilar <josueaguilar1198@gmail.com>
2020-05-28 17:41:28 -0600

    bug redireccion

--- funcionalidades/controlador-landing.js
+++ funcionalidades/controlador-landing.js
@@ -1,15 +1,6 @@
-$(document).ready(function(){
-    axios({
-        method:'GET',
-        url:'https://api-unahambre.herokuapp.com/api_producto/banners'
-    }).then(res=>{
-        console.log(res);
-        let img_banner=`<img src="img/banner.png" class="img-banner" onclick="redireccionar()" data-plan="${res.data.items.Plan_idPlan}">`
-        document.getElementById('banner').innerHTML= img_banner;
-    }).catch(err=>{
-        console.log(err);
-    });
-});
+// $(document).ready(function(){
+    
+// });
 
 $("#redireccionar-n").click(function(){
     sessionStorage.setItem('stepAnterior', 'btn-negocio' )  //Codigo sujeto a cambios
@@ -38,6 +29,17 @@ document.addEventListener("DOMContentLoaded", () => {
     }).catch(err => {
         console.log(err)
     })
+
+    axios({
+        method:'GET',
+        url:'https://api-unahambre.herokuapp.com/api_producto/banners'
+    }).then(res=>{
+        console.log(res);
+        let img_banner=`<img src="img/banner.png" class="img-banner" onclick="redireccionar()" data-plan="${res.data.items.Plan_idPlan}">`
+        document.getElementById('banner').innerHTML= img_banner;
+    }).catch(err=>{
+        console.log(err);
+    });
 })
 
 function redireccionar() {
